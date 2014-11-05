3bbf680a60bea181295c5070b1793a3155b6257e
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2014-11-05 12:14:04 +0100

    added opening windows at click on the statusitem

--- FastTTS/AppDelegate.swift
+++ FastTTS/AppDelegate.swift
@@ -11,9 +11,33 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    lazy var popover: NSWindowController = PopOver(windowNibName: "PopOver")
+
+    var statusBar = NSStatusBar.systemStatusBar()
+    var statusBarItem : NSStatusItem = NSStatusItem()
+    var menu: NSMenu = NSMenu()
+    var menuItem : NSMenuItem = NSMenuItem()
+    
     let speechSynth = NSSpeechSynthesizer(voice: NSSpeechSynthesizer.defaultVoice())
 
+    override func awakeFromNib() {
+        statusBarItem = statusBar.statusItemWithLength(-1)
+        statusBarItem.title = "FastTTS"
+        statusBarItem.target = self
+        statusBarItem.action = Selector("openWindow:")
+        
+        //Add menuItem to menu
+        menuItem.title = "Clicked"
+        menuItem.action = Selector("setWindowVisible:")
+        menuItem.keyEquivalent = ""
+        menu.addItem(menuItem)
+    }
+    
+    @IBAction func openWindow(sender: AnyObject) {
+        NSApplication.sharedApplication().activateIgnoringOtherApps(true)
+        popover.showWindow(nil)
+    }
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         
     }
---
+++ FastTTS/PopOver.swift
@@ -0,0 +1,19 @@
+//
+//  PopOver.swift
+//  FastTTS
+//
+//  Created by Gabriele Carrettoni on 05/11/14.
+//  Copyright (c) 2014 Gabriele Carrettoni. All rights reserved.
+//
+
+import Cocoa
+
+class PopOver: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
